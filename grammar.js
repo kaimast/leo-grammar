04001364d5a41601e89c4506831bc40ae4620993
Kai Mast <kai@provable.com>
2025-03-25 21:24:39 -0700

    Initial commit

---
+++ grammar.js
@@ -0,0 +1,81 @@
+
+module.exports = grammar({
+  name: 'leo',
+
+  extras: $ => [/\s/, /\t/, $.comment],
+
+  rules: {
+    source_file: $ => seq(
+      'program', $.program_name, '{',
+        $.program_member,
+      '}'
+    ),
+
+    program_name: $ => /[a-zA-Z_][a-zA-Z0-9_]*\.aleo/,
+
+    // Statements
+    program_member: $ =>
+      choice(
+        $.transition,
+      ),
+
+    // Variable declaration: let x = 42
+    variable_declaration: $ => seq(
+      'let', $.identifier, ':', $.identifier, '=', $.expression
+    ),
+
+    // Transiton declartion: transition add(x, y) { return x + y }
+    transition: $ => seq(
+      'transition', $.identifier, '(', $.parameter_list, ')',
+       optional(seq('->', $.identifier)),
+       $.block
+    ),
+
+    return: $ => seq('return', $.expression),
+
+    // Expression statement: x = 42 or call(foo, bar)
+    statement: $ => seq(choice(
+      $.variable_declaration, $.function_call, $.return
+    ), ';'),
+
+    // Expressions
+    expression: $ => choice(
+      $.literal, $.identifier, $.binary_expression, $.function_call
+    ),
+
+    // Literal values like numbers, strings, etc.
+    literal: $ => choice($.number, $.string),
+
+    // Identifier: a simple variable or function name
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+    // Numbers (integer or floating-point)
+    number: $ => /\d+(\.\d+)?/,
+
+    // Strings (single or double quoted)
+    string: $ => /"[^"]*"/,
+
+    // Binary expression: x + y, a * b, etc.
+    binary_expression: $ => prec.left(seq($.expression, choice('+', '-', '*', '/', '%'), $.expression)),
+
+    // Function call: foo(bar, baz)
+    function_call: $ => seq($.identifier, '(', optional($.argument_list), ')'),
+    typename: $ => $.identifier,
+
+    // Function arguments
+    argument_list: $ => seq($.argument, repeat(seq(',', $.argument))),
+
+    argument: $ => $.identifier,
+
+    // Parameters in a function declaration
+    parameter_list: $ => seq($.parameter, repeat(seq(',', $.parameter))),
+
+    parameter: $ => seq(optional('public'), $.identifier, ':', $.typename),
+
+
+    // Block of code enclosed by braces
+    block: $ => seq('{', repeat($.statement), '}'),
+
+    comment: $ => seq('//', /.*/),
+  }
+})
---
+++ queries/highlights.scm
@@ -0,0 +1,7 @@
+; highlights.scm
+
+(comment) @comment
+"transition" @keyword
+"return" @keyword
+"program" @keyword
+(identifier) @identifier
